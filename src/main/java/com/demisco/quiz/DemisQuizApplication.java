306d77e37f4911c1c677cf278d6ef149f061eb09
Hamidreza Mohammadi <makenkap@gmail.com>
2025-02-21 12:02:38 +0330

    Initiate

---
+++ src/main/java/com/demisco/quiz/DemisQuizApplication.java
@@ -0,0 +1,13 @@
+package com.demisco.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemisQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemisQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demisco/quiz/DemisQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demisco.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemisQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
